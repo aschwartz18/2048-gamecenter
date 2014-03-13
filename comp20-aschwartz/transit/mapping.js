8e8a59621ca18ac2964b20cc2bbbed243df3a3c1
aschwartz18 <allison.schwartz@tufts.edu>
2014-03-13 04:02:51 -0400

    addded message 3

--- comp20-aschwartz/transit/mapping.js
+++ comp20-aschwartz/transit/mapping.js
@@ -18,7 +18,7 @@ function getMyLocation() {
         navigator.geolocation.getCurrentPosition(function(position) {
             myLat = position.coords.latitude;
             myLng = position.coords.longitude;
-            console.log(myLat + myLong);
+            console.log(myLat + myLng);
             renderMap();
         });
     }
