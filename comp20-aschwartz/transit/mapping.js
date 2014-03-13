3d21837f7b2ae64ab4a82a37071e9078bfa671e6
aschwartz18 <allison.schwartz@tufts.edu>
2014-03-13 04:14:57 -0400

    testing

--- comp20-aschwartz/transit/mapping.js
+++ comp20-aschwartz/transit/mapping.js
@@ -1,41 +1,91 @@
-myLat = 0;
-myLng = 0;
-var request = new XMLHttpRequest();
-var me = new google.maps.LatLng(myLat, myLng);
-var myOptions = {zoom: 13, center: me, mapTypeId: google.maps.MapTypeId.ROADMAP};
-var map;
-var marker;
-var infowindow = new google.maps.InfoWindow();
+var myLat = 0;
+			var myLng = 0;
+			var request = new XMLHttpRequest();
+			var me = new google.maps.LatLng(myLat, myLng);
+			var myOptions = {
+						zoom: 13, // The larger the zoom number, the bigger the zoom
+						center: me,
+						mapTypeId: google.maps.MapTypeId.ROADMAP
+					};
+			var map;
+			var marker;
+			var infowindow = new google.maps.InfoWindow();
+			var places;
+			
+			function init()
+			{
+				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+				getMyLocation();
+			}
+			
+			function getMyLocation()
+			{
+				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+					navigator.geolocation.getCurrentPosition(function(position) {
+						myLat = position.coords.latitude;
+						myLng = position.coords.longitude;
+						renderMap();
+					});
+				}
+				else {
+					alert("Geolocation is not supported by your web browser.  What a shame!");
+				}
+			}
 
-function init() {
-	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-	console.log("testing one");
-	getMyLocation();
-}
+			function renderMap()
+			{
+				me = new google.maps.LatLng(myLat, myLng);
+				
+				// Update map and go there...
+				map.panTo(me);
+	
+				// Create a marker
+				marker = new google.maps.Marker({
+					position: me,
+					title: "Here I Am!"
+				});
+				marker.setMap(map);
+					
+				// Open info window on click of marker
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(marker.title);
+					infowindow.open(map, marker);
+				});
+				
+				// Calling Google Places API
+				var request = {
+					location: me,
+					radius: '500',
+					types: ['food']
+				};
+				service = new google.maps.places.PlacesService(map);
+				service.search(request, callback);
+			}
+			
+			// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
+			function callback(results, status)
+			{
+				if (status == google.maps.places.PlacesServiceStatus.OK) {
+					alert("Got places back!");
+					places = results;
+					for (var i = 0; i < results.length; i++) {
+						createMarker(results[i]);
+					}
+				}
+			}
+			
+			function createMarker(place)
+			{
+				var placeLoc = place.geometry.location;
+				var marker = new google.maps.Marker({
+					map: map,
+					position: place.geometry.location
+				});
 
-function getMyLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            myLat = position.coords.latitude;
-            myLng = position.coords.longitude;
-            console.log(myLat + myLng);
-            renderMap();
-        });
-    }
-    else {
-        alert("Geolocation is not supported by your web browser.  What a shame!");
-    }
-}
-
-function renderMap() {
-	me = new google.maps.LatLng(myLat, myLng);
-	map.panTo(me);
-	marker = new google.maps.Marker({position: me, title: "I am here at " + myLat + ", " + myLng + ""});
-	marker.setMap(map);
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
-	});
-}
-
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.close();
+					infowindow.setContent(place.name);
+					infowindow.open(map, this);
+				});
+      }
 
