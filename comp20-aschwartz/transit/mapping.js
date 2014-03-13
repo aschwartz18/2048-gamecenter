1252b5e768076e1fcd032138ba745625b36c1bda
aschwartz18 <allison.schwartz@tufts.edu>
2014-03-13 04:35:40 -0400

    changed order to attempt to fix google problem

--- comp20-aschwartz/transit/mapping.js
+++ comp20-aschwartz/transit/mapping.js
@@ -1,3 +1,4 @@
+var infowindow = new google.maps.InfoWindow();
 myLat = 0;
 myLng = 0;
 var request = new XMLHttpRequest();
@@ -5,12 +6,11 @@ var me;
 var myOptions = {zoom: 13, center: me, mapTypeId: google.maps.MapTypeId.ROADMAP};
 var map;
 var marker;
-var infowindow;
+
 
 function init() {
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	console.log("testing one");
-	infowindow = new google.maps.InfoWindow();
 	getMyLocation();
 }
 
