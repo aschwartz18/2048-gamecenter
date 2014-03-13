75c7a995444415eae2165a99a8dec2ed7e267abd
aschwartz18 <allison.schwartz@tufts.edu>
2014-03-13 06:18:21 -0400

    me marker

--- comp20-aschwartz/transit/mapping.js
+++ comp20-aschwartz/transit/mapping.js
@@ -1,52 +1,40 @@
+var myLat = 0;
+var myLng = 0;
 var request = new XMLHttpRequest();
-var infowindow = new google.maps.InfoWindow();
-myLat = 0;
-myLng = 0;
-var me;
+var me = new google.maps.LatLng(myLat, myLng);
 var myOptions = {zoom: 13, center: me, mapTypeId: google.maps.MapTypeId.ROADMAP};
 var map;
 var marker;
-
-
+var infowindow = new google.maps.InfoWindow();
+var places;
+			
 function init() {
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	getMyLocation();
 }
-
+			
 function getMyLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            myLat = position.coords.latitude;
-            myLng = position.coords.longitude;
-            me = new google.maps.LatLng(myLat, myLng);
-            console.log(myLat + "," + myLng);
-            parse();
-            renderMap();
-        });
-    }
-    else {
-        alert("Geolocation is not supported by your web browser.  What a shame!");
-    }
+	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by your web browser.  What a shame!");
+	}
 }
 
 function renderMap() {
 	me = new google.maps.LatLng(myLat, myLng);
 	map.panTo(me);
-	marker = new google.maps.Marker({position: me, title: "I am here at " + myLat + ", " + myLng + ""});
+	marker = new google.maps.Marker({
+	position: me,
+	title: "I am here at " + myLat + ", " + myLng + ""});
 	marker.setMap(map);
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(marker.title);
-		infowindow.open(map, marker);
-	});
+	google.maps.event.addListener(marker, 'click', function() {infowindow.setContent(marker.title); infowindow.open(map, marker);});
 }
-
-function parse(){
-	var line = JSON.parse('line');
-	console.log(line);
-}
-
-
-
 
 
 
