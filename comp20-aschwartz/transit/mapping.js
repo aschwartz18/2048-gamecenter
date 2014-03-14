7a34b24c32e49ac9f9b01cfb99deee1b6d56916f
aschwartz18 <allison.schwartz@tufts.edu>
2014-03-13 22:31:19 -0400

    Changed inequality

--- comp20-aschwartz/transit/mapping.js
+++ comp20-aschwartz/transit/mapping.js
@@ -124,7 +124,7 @@ function findClosest() {
                 Math.cos(toRad(myLat)) * Math.cos(toRad(parsed[i]['lat'])) * 
                 Math.sin(dLng/2) * Math.sin(dLng/2);  
 		var c = R * (2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))); 
-		if (c < closestDistance) {
+		if (c > closestDistance) {
 			closestDistance = c;
 			closestID = parsed[i]['id'];
 		}
